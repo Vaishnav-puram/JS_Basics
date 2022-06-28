ee4b6d1e2bf87d2af93aeac1254c8c23abbc95cd
Vaishnav Puram@DESKTOP-1GKDG5O <puramv307@gmail.com>
2022-06-28 16:36:27 +0530

    made changes in objectDestructuring.js

--- ES6/objectDestructuring.js
+++ ES6/objectDestructuring.js
@@ -1,21 +1,21 @@
-//by this it is possible to unpack values from arrays or properties from objects into distinct variables
-let obj={
-    name:"vaishnav",
-    age:23,
-    city:"hyderabad"
-}
-console.log(`Name is ${obj.name} age is ${obj.age} lives in ${obj.city}`) //Name is vaishnav age is 23 lives in hyderabad
+// //by this it is possible to unpack values from arrays or properties from objects into distinct variables
+// let obj={
+//     name:"vaishnav",
+//     age:23,
+//     city:"hyderabad"
+// }
+// console.log(`Name is ${obj.name} age is ${obj.age} lives in ${obj.city}`) //Name is vaishnav age is 23 lives in hyderabad
 
-//with objectdestructuring
-let {name,age,city}=obj //order is important to match with properties of object
-console.log(`Name is ${name} age is ${age} lives in ${city}`) ///Name is vaishnav age is 23 lives in hyderabad
-//or 
-// let name,age,city;
-// ({name,age,city}=obj)
+// //with objectdestructuring
+// let {name,age,city}=obj //order is important to match with properties of object
+// console.log(`Name is ${name} age is ${age} lives in ${city}`) ///Name is vaishnav age is 23 lives in hyderabad
+// //or 
+// // let name,age,city;
+// // ({name,age,city}=obj)
 
-//second property of object destructuring 
-let{name:myname,age:myage,city:mycity}=obj;
-console.log(`Name is ${myname} age is ${myage} lives in ${mycity}`) //Name is vaishnav age is 23 lives in hyderabad
+// //second property of object destructuring 
+// let{name:myname,age:myage,city:mycity}=obj;
+// console.log(`Name is ${myname} age is ${myage} lives in ${mycity}`) //Name is vaishnav age is 23 lives in hyderabad
 
 
 let obj1={
@@ -28,4 +28,4 @@ let obj1={
     }
 }
 let{Name,Age,City,fav}=obj1;
-console.log(`Name is ${myname} age is ${myage} lives in ${mycity} and interests ${fav.one} ${fav.two}`) //Name is vaishnav age is 23 lives in hyderabad and interests cars movies
+console.log(`Name is ${Name} age is ${Age} lives in ${City} and interests ${fav.one} ${fav.two}`) //Name is vaishnav age is 23 lives in hyderabad and interests cars movies
